44f43507a01c9ad3b658346addf41e29527fd847
Zetciak <97193808+Zetciak@users.noreply.github.com>
2022-03-24 20:50:41 +0100

    fix#02

--- components/indexPage/galaxyCommunity/galaxyCommunity.js
+++ components/indexPage/galaxyCommunity/galaxyCommunity.js
@@ -13,83 +13,101 @@ import useStyles from './galaxyCommunityStyle';
 
 //Script
 function GalaxyCommunity() {
-  //Style
-  const classes = useStyles();
+	//Style
+	const classes = useStyles();
 
-  return (
-    <>
-      <div className={classes.mainDiv}>
-        <div className={classes.bgDiv}></div>
-        <div className={classes.centerDiv}>
-          <div className={classes.imageDiv}>
-            <Image src={bottomImage} alt="" />
-          </div>
-          <div className={classes.buttonsDiv}>
-            <Typography className={classes.socialsTitle}>
-              Check our galaxy community
-            </Typography>
-            <div className={classes.buttons}>
-              <Button
-                variant="contained"
-                className={classes.rightSectionBtn}
-                href="https://discord.gg/terraaliens"
-                target="_blank"
-              >
-                <div className={classes.rightSectionBtnIconDc}>
-                  <Image src={discord} alt="" />
-                </div>
-                <Typography>Discord</Typography>
-              </Button>
-              <Button
-                variant="contained"
-                className={classes.rightSectionBtn}
-                href="https://twitter.com/TerraAliensNFT"
-                target="_blank"
-              >
-                <div className={classes.rightSectionBtnIconTwt}>
-                  <Image src={twitter} alt="" />
-                </div>
-                <Typography className={classes.twitterUpper}>
-                  Twitter
-                </Typography>
-              </Button>
-              <Button variant="contained" className={classes.rightSectionBtn}>
-                <div className={classes.rightSectionBtnIconMed}>
-                  <Image src={medium} alt="" />
-                </div>
-                <Typography className={classes.mediumUpper}>Medium</Typography>
-              </Button>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className={classes.mobileButtons}>
-        <Typography className={classes.socialsTitle}>
-          Check our galaxy community
-        </Typography>
-        <div className={classes.buttons}>
-          <Button variant="contained" className={classes.rightSectionBtn}>
-            <div className={classes.rightSectionBtnIconDc}>
-              <Image src={discord} alt="" />
-            </div>
-            <Typography>Discord</Typography>
-          </Button>
-          <Button variant="contained" className={classes.rightSectionBtn}>
-            <div className={classes.rightSectionBtnIconTwt}>
-              <Image src={twitter} alt="" />
-            </div>
-            <Typography className={classes.twitterUpper}>Twitter</Typography>
-          </Button>
-          <Button variant="contained" className={classes.rightSectionBtn}>
-            <div className={classes.rightSectionBtnIconMed}>
-              <Image src={medium} alt="" />
-            </div>
-            <Typography className={classes.mediumUpper}>Medium</Typography>
-          </Button>
-        </div>
-      </div>
-    </>
-  );
+	return (
+		<>
+			<div className={classes.mainDiv}>
+				<div className={classes.bgDiv}></div>
+				<div className={classes.centerDiv}>
+					<div className={classes.imageDiv}>
+						<Image src={bottomImage} alt="" />
+					</div>
+					<div className={classes.buttonsDiv}>
+						<Typography className={classes.socialsTitle}>
+							Check our galaxy community
+						</Typography>
+						<div className={classes.buttons}>
+							<Button
+								variant="contained"
+								className={classes.rightSectionBtn}
+								href="https://discord.gg/terraaliens"
+								target="_blank"
+							>
+								<div className={classes.rightSectionBtnIconDc}>
+									<Image src={discord} alt="" />
+								</div>
+								<Typography>Discord</Typography>
+							</Button>
+							<Button
+								variant="contained"
+								className={classes.rightSectionBtn}
+								href="https://twitter.com/TerraAliensNFT"
+								target="_blank"
+							>
+								<div className={classes.rightSectionBtnIconTwt}>
+									<Image src={twitter} alt="" />
+								</div>
+								<Typography className={classes.twitterUpper}>
+									Twitter
+								</Typography>
+							</Button>
+							<Button
+								variant="contained"
+								className={classes.rightSectionBtn}
+							>
+								<div className={classes.rightSectionBtnIconMed}>
+									<Image src={medium} alt="" />
+								</div>
+								<Typography className={classes.mediumUpper}>
+									Medium
+								</Typography>
+							</Button>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div className={classes.mobileButtons}>
+				<Typography className={classes.socialsTitle}>
+					Check our galaxy community
+				</Typography>
+				<div className={classes.buttons}>
+					<Button
+						variant="contained"
+						className={classes.rightSectionBtn}
+					>
+						<div className={classes.rightSectionBtnIconDc}>
+							<Image src={discord} alt="" />
+						</div>
+						<Typography>Discord</Typography>
+					</Button>
+					<Button
+						variant="contained"
+						className={classes.rightSectionBtn}
+					>
+						<div className={classes.rightSectionBtnIconTwt}>
+							<Image src={twitter} alt="" />
+						</div>
+						<Typography className={classes.twitterUpper}>
+							Twitter
+						</Typography>
+					</Button>
+					<Button
+						variant="contained"
+						className={classes.rightSectionBtn}
+					>
+						<div className={classes.rightSectionBtnIconMed}>
+							<Image src={medium} alt="" />
+						</div>
+						<Typography className={classes.mediumUpper}>
+							Medium
+						</Typography>
+					</Button>
+				</div>
+			</div>
+		</>
+	);
 }
 
 export default GalaxyCommunity;
